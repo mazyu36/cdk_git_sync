4b35eadee7d8375df0561eda7ea97a4a764deeff
maz <yiyth.fcb6@gmail.com>
2023-11-30 16:50:42 +0900

    Queueを追加

--- lib/cdk_git_sync-stack.ts
+++ lib/cdk_git_sync-stack.ts
@@ -12,6 +12,11 @@ export class CdkGitSyncStack extends cdk.Stack {
       visibilityTimeout: cdk.Duration.seconds(300)
     });
 
+    // 追加
+    const newQueue = new sqs.Queue(this, 'CdkGitTest', {
+      visibilityTimeout: cdk.Duration.seconds(300)
+    });
+
     // Lambda functionを追加
     new lambda.Function(this, 'LambdaFunction', {
       runtime: lambda.Runtime.PYTHON_3_11,
